e550a4e0cce7c2110319231c5df3fef53b02cb5f
김광민 <kmkim4238@gmail.com>
2023-04-18 23:07:36 +0900

    230418 first commit, 파일 구조 및 custom hooks 작성

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,51 @@
+import { useCallback, useState } from "react";
+import { API_BASE_URL } from "../global/constants";
+
+type MethodType = "GET" | "POST" | "PATCH" | "DELETE";
+
+const useFetch = () => {
+  const [isLoading, setIsLoading] =
+    useState<React.SetStateAction<boolean>>(false);
+  const [error, setError] = useState<React.SetStateAction<boolean>>(false);
+
+  const sendRequest = useCallback(
+    async (
+      url: string,
+      method: MethodType = "GET",
+      headers = {},
+      body: any
+    ) => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(`${API_BASE_URL + url}`, {
+          method,
+          headers,
+          body,
+        });
+
+        const responseData: any = response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        return responseData;
+      } catch (err: any) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(false);
+  };
+
+  return { isLoading, error, sendRequest, clearError };
+};
+
+export default useFetch;
